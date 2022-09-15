b7c4dead88ebdf04953c1af140d0db8c4de89f19
Russell Smith <ukd1@users.noreply.github.com>
2022-09-15 13:18:49 -0700

    Try really rendering the issues --> html

---
+++ lol.rb
@@ -0,0 +1,15 @@
+puts '<html>'
+puts '<head>'
+puts '<title>RC Creative coding lol</title>'
+puts '</head>'
+puts '<body>'
+puts '<h1>RC Creative coding list of things</h1>'
+
+Dir.glob("sketches/*.md") do |file|
+  puts "<h2>#{file}</h2>"
+  puts File.read(file)
+  puts "<br>"
+end
+
+puts '</body>'
+puts '</html>'
