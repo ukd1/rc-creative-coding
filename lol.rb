93bdf98886de2fa3b8f449b291c67b25607beb36
Julian DeGroot-Lutzner <jdegrootlutzner@gmail.com>
2022-09-15 15:58:44 -0500

    Add JS markdown renderer

--- lol.rb
+++ lol.rb
@@ -1,8 +1,10 @@
 puts '<html>'
 puts '<head>'
+puts '<meta charset="utf-8"/>'
+puts '<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>'
 puts '<title>RC Creative coding lol</title>'
 puts '</head>'
-puts '<body>'
+puts '<body id="content">'
 puts '<h1>RC Creative coding list of things</h1>'
 
 Dir.glob("sketches/*.md").sort.reverse.each do |file|
@@ -11,5 +13,13 @@ Dir.glob("sketches/*.md").sort.reverse.each do |file|
   puts "<br>"
 end
 
+puts <<-EOF
+<script>
+  marked.setOptions({ gfm: true, breaks: true});
+
+  document.getElementById('content').innerHTML =
+    marked.parse(  document.getElementById('content').innerHTML);
+</script>
+EOF
 puts '</body>'
 puts '</html>'
