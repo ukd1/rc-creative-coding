de7f992978484e48018005f3bed4a9d1707cfe8b
Russell Smith <ukd1@users.noreply.github.com>
2022-09-15 17:21:43 -0700

    Move to less-terrible way of outputting html with just stdlib

--- lol.rb
+++ lol.rb
@@ -1,33 +1,69 @@
-puts '<html>'
-puts '<head>'
-puts '<meta charset="utf-8"/>'
-puts '<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>'
-puts '<title>RC Creative coding lol</title>'
-puts '</head>'
-puts '<body id="content">'
-puts '<h1>RC Creative coding list of things</h1>'
-
+require 'erb'
 
 def get_sketch_id(file)
   File.basename(file).split('.')[0].to_i
 end
+
 # we have to sort like this as we want a "natural sort" based on the actual number
 # not the text of the filename
-Dir.glob("sketches/*.md").sort_by{|file| get_sketch_id(file) }.reverse.each do |file|
-  id = get_sketch_id(file)
-  puts "<h2 id='#{id}'>#{file}</h2> <a href='##{id}'>🔗</a>"
-  puts "<div class='md'>#{File.read(file)}</div>"
-  puts "<br>"
-end
+sketches = Dir.glob("sketches/*.md").sort_by{|file| get_sketch_id(file) }.reverse
 
-puts <<-EOF
-<script>
-  marked.setOptions({gfm: true, breaks: true});
 
-  document.querySelectorAll('.md').forEach((block) => {
-    block.innerHTML = marked.parse(block.innerHTML);
-  });
-</script>
+# ERB template for the main page
+#
+main_template = ERB.new <<-EOF
+<html>
+<head>
+  <title>RC Creative Coding Archive</title>
+  <meta charset="utf-8"/>
+  <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+</head>
+<body>
+  <h1>🐙 RC Creative Coding Archive 👋</h1>
+  <p>This is an archive of creative coding things folks at RC have done (or found)! Please enjoy and share</p>
+
+  <p>
+    <a href="https://github.com/ukd1/rc-creative-coding/issues/new">Add something!</a>
+    &mdash;
+    <a href="https://github.com/ukd1/rc-creative-coding">The code</a>
+    &mdash;
+    <a href="https://www.recurse.com">Recurse Center</a>
+  </p>
+
+  <br>
+
+  <%= sketches_html %>
+
+  <script>
+    marked.setOptions({gfm: true, breaks: true});
+
+    document.querySelectorAll('.md').forEach((block) => {
+      block.innerHTML = marked.parse(block.innerHTML);
+    });
+  </script>
+</body>
+</html>
 EOF
-puts '</body>'
-puts '</html>'
+
+# ERB template for the individual sketchs
+#
+sketch_template = ERB.new <<-EOF
+  <div id="sketch_<%= id %>">
+    <h2><%= file %></h2> <a href='sketch_#<%= id %>'>🔗</a>
+    <div class='md'><%= File.read(file) %></div>
+    <br>
+  </div>
+EOF
+
+
+# map over each sketch
+sketches_html = sketches.map do |file|
+  id = get_sketch_id(file)
+
+  # and render the template...
+  sketch_template.result(binding)
+end.join("\n") # then join each sketch by new lines... 😅
+
+# just puts the output, as we're prolly just gonna write to a file
+puts main_template.result(binding)
+
